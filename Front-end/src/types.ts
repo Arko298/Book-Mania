0c63a625f9ce3337034bd69c8e93458a301cdeed
Arko298 <choudhurysayantan03@gmail.com>
2024-10-05 20:10:28 +0530

    Interface are being seperated

---
+++ Front-end/src/types.ts
@@ -0,0 +1,22 @@
+export interface Book {
+    id: string
+    title: string
+    author: string
+    cover: string
+    rating: number
+    ratingCount: number
+    description: string
+    pageLength: number
+    language: string
+    publisher: string
+    publishingDate: string
+  }
+export interface RecommendedBook {
+    id: string
+    title: string
+    author: string
+    cover: string
+    rating: number
+    ratingCount: number
+    description: string
+  }
