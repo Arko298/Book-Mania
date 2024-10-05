1d15aa34c6c4200fa350638638a2c7f9094108d9
Arko298 <choudhurysayantan03@gmail.com>
2024-10-05 20:11:55 +0530

    ReadingList are formed

--- Front-end/src/components/ReadingList/ReadingList.tsx
+++ Front-end/src/components/ReadingList/ReadingList.tsx
@@ -1,1 +1,26 @@
 //Reading list books in tsx 
+
+import Footer from "../Footer/Footer"
+
+const ReadingList = () => {
+  return (
+    <div className="bg-[#7F642E] p-5 flex justify-between">
+      <div className="flex-1 mx-2 bg-white p-4 rounded">
+        <h2 className="text-center">Section 1</h2>
+        {/* Add book suggestions for Section 1 */}
+      </div>
+      <div className="flex-1 mx-2 bg-white p-4 rounded">
+        <h2 className="text-center">Section 2</h2>
+        {/* Add book suggestions for Section 2 */}
+      </div>
+      <div className="flex-1 mx-2 bg-white p-4 rounded">
+        <h2 className="text-center">Section 3</h2>
+        {/* Add book suggestions for Section 3 */}
+      </div>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default ReadingList
