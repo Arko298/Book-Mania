e9aa0585e9645bbde0f547f37255d95e09714f07
Arko298 <choudhurysayantan03@gmail.com>
2024-10-05 20:11:38 +0530

    Footer is added

--- Front-end/src/components/Footer/Footer.tsx
+++ Front-end/src/components/Footer/Footer.tsx
@@ -1,28 +1,25 @@
 export default function Footer(){
+    const footerItems = ["Help", "Books", "Read", "Download", "Like", "Bookmark"];
+    
     return(
-        <>
-        <footer className="border-gray-4 bg-paper dark:bg-dark-layer-bg mx-4 mt-[50px] border-t p-5 dark:mx-0 dark:border-none md:mt-24 text-label-2 dark:text-dark-label-2">
+        <footer className="border-gray-4 bg-paper dark:bg-dark-layer-bg mx-4 mt-[50px] border-t p-5 dark:mx-0 dark:border-none md:mt-24 text-label-2 dark:text-dark-label-2" aria-label="Footer">
             <div className="w-full">
                 <div className="m-auto max-w-6xl">
-                    <div className="text-xs md:flex md:text-sm">
+                    <nav className="text-xs md:flex md:text-sm">
                         <div>
-                            {/* Copyrights */}
                             Copyrights
                         </div>
                         <div>
-                            {/* items in the footer to be flex-wrapped */}
-                            Help | Books | Read | Download | Like | Bookmark
+                            {footerItems.map((item, index) => (
+                                <span key={index}>{item} | </span>
+                            ))}
                         </div>
                         <div>
-                            {/** region but not using right now */}
                             India
                         </div>
-                    </div>
-
+                    </nav>
                 </div>
-
             </div>
         </footer>
-        </>
     )
 }
